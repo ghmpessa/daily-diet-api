7c53b5e47b33284830efd46f6365eca586b8e66d
Gustavo Pessa <ghmpessa@gmail.com>
2024-06-27 22:25:44 -0300

    feat: ensure meals route creates a meal

---
+++ src/routes/meals.ts
@@ -0,0 +1,36 @@
+import { randomUUID } from 'node:crypto'
+import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import { knex } from '../database'
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
+
+export const mealsRoute = async (app: FastifyInstance) => {
+  app.post(
+    '/',
+    {
+      preHandler: [checkSessionIdExists]
+    },
+    async (req, res) => {
+      const { sessionId } = req.cookies
+
+      const mealsSchema = z.object({
+        title: z.string(),
+        description: z.string(),
+        date: z.string().datetime(),
+        isInsideTheDiet: z.boolean()
+      })
+
+      const { title, description, date, isInsideTheDiet } = mealsSchema.parse(req.body)
+
+      await knex('meals').insert({
+        id: randomUUID(),
+        title,
+        description,
+        date,
+        is_inside_the_diet: isInsideTheDiet,
+        user_id: sessionId
+      })
+
+      res.status(201).send()
+    })
+}
