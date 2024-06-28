d3d1d872c459ce0710f67af9150d6b8ddfc74605
Gustavo Pessa <ghmpessa@gmail.com>
2024-06-27 22:26:12 -0300

    feat: add meals plugin to app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import fastify from 'fastify'
-import { usersRoutes } from './routes/users'
+import { usersRoutes, mealsRoute } from './routes'
 import cookie from '@fastify/cookie'
+
 export const app = fastify()
 
 app.register(cookie)
+app.register(mealsRoute, {
+  prefix: 'meals'
+})
 app.register(usersRoutes, {
   prefix: 'users'
 })
