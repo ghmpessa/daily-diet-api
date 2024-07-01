d8d1c7c07f157d05acb283e87746d656ff4ec800
Gustavo Pessa <ghmpessa@gmail.com>
2024-07-01 18:36:30 -0300

    test: ensure get /meals route returns list of meals

--- test/meals.spec.ts
+++ test/meals.spec.ts
@@ -1,7 +1,18 @@
 import { execSync } from 'node:child_process'
 import { app } from '../src/app'
 import request from 'supertest'
-import { afterAll, beforeAll, beforeEach, describe, it } from 'vitest'
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from 'vitest'
+
+const makeSut = async () => {
+  const user = await request(app.server).post('/users').send({
+    name: 'username',
+    avatarUrl: ''
+  })
+
+  const cookie = user.get('Set-Cookie')
+
+  return cookie
+}
 
 describe('Users', () => {
   beforeAll(async () => {
@@ -18,12 +29,7 @@ describe('Users', () => {
   })
 
   it('should create a new meal', async () => {
-    const user = await request(app.server).post('/users').send({
-      name: 'username',
-      avatarUrl: ''
-    })
-
-    const cookie = user.get('Set-Cookie')
+    const cookie = await makeSut()
 
     await request(app.server)
       .post('/meals')
@@ -35,5 +41,40 @@ describe('Users', () => {
         isInsideTheDiet: false,
       })
       .expect(201)
+  })
+
+  it('should list all user meals', async () => {
+    const user = await request(app.server)
+      .post('/users')
+      .send({
+        name: 'username',
+      })
+
+    const cookie = user.get('Set-Cookie')
+
+    const date = new Date(Date.now()).toISOString()
+
+    await request(app.server)
+      .post('/meals')
+      .send({
+        title: 'Bacon Duplo',
+        description: 'Bacon Duplo do Aconchego da gula',
+        date,
+        isInsideTheDiet: false,
+      })
+      .set('Cookie', cookie!)
+
+    const { body: { meals }, status } = await request(app.server)
+      .get('/meals')
+      .set('Cookie', cookie!)
+
+    expect(status).toEqual(200)
+    expect(meals).toEqual([
+      expect.objectContaining({
+        title: 'Bacon Duplo',
+        description: 'Bacon Duplo do Aconchego da gula',
+        date,
+      })
+    ])
   })
 })
